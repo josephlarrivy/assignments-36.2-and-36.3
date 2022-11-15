ef12a857586e58b38581d3b22e5cec673c44f8d8
josephlarrivy <joseph.larrivy@gmail.com>
2022-11-15 14:23:49 -0600

    first commit

---
+++ async-await/part-1-number-facts/number-facts.js
@@ -0,0 +1,33 @@
+// Part 1: Number Facts
+// Make a request to the Numbers API(http://numbersapi.com/) to get a fact about your favorite number. (Make sure you get back JSON by including the json query key, specific to this API.
+
+// Figure out how to get data on multiple numbers in a single request. Make that request and when you get the data back, put all of the number facts on the page.
+
+// Use the API to get 4 facts on your favorite number.Once you have them all, put them on the page.It’s okay if some of the facts are repeats.
+
+// (Note: You’ll need to make multiple requests for this.)
+
+let baseUrl = 'http://numbersapi.com/'
+
+async function getNumberFacts() {
+    let response = await $.getJSON(`${baseUrl}${number}?json`);
+    console.log(response)
+}
+let number = Math.floor(Math.random() * 20)
+getNumberFacts(number);
+
+
+let numbers = [12,77,43];
+async function getMultiple() {
+    let response = await $.getJSON(`${baseUrl}${number}?json`);
+    console.log(response);
+}
+getMultiple();
+
+
+let arr = [];
+async function getMultipleAgain() {
+    let resp = await Promise.all(
+    arr.push () => $.getJSON(`${baseUrl}${num}`)
+    )
+}
---
+++ async-await/part-2-deck-of-cards/deck_of_cards.js
@@ -0,0 +1,47 @@
+// Make a request to the Deck of Cards API to request a single card from a newly shuffled deck.Once you have the card, console.log the value and the suit(e.g. “5 of spades”, “queen of diamonds”).
+
+// Make a request to the deck of cards API to request a single card from a newly shuffled deck.Once you have the card, make a request to the same API to get one more card from the same deck.
+
+// Once you have both cards, console.log the values and suits of both cards.
+
+// Build an HTML page that lets you draw cards from a deck.When the page loads, go to the Deck of Cards API to create a new deck, and show a button on the page that will let you draw a card.Every time you click the button, display a new card, until there are no cards left in the deck.
+
+
+$(function () {
+    let baseUrl = 'https://deckofcardsapi.com/api/deck';
+
+    async function requestOne() {
+        let resp = await $.getJSON(`${baseUrl}/new/draw/`);
+        let {suit,value} = resp.cards[0];
+        console.log(`${value} of ${suit}`);
+    }
+
+    async function requestTwo() {
+        let firstCard = await $.getJSON(`${baseUrl}/new/draw/`);
+        let deckId = firstCard.deck_id;
+        let secondCard = await $.getJSON(`${baseUrl}/${deckId}/draw`);
+        cardData = [firstCard, secondCard];
+        for (data in cardData) {
+            let {suit,value} = data.cards;
+            console.log(`${value} of ${suit}`)
+        }
+    }
+
+    async function part3() {
+        let $button = $('button');
+        let $cardArea = $('#card-area');
+
+        let deckData = await $.getJSON(`${baseUrl}/new/shuffle/`);
+        $button.show().on('click', async function () {
+            let cardData = await $.getJSON(`${baseUrl}/${deckData.deck_id}/draw/`);
+            let cardSrc = cardData.cards[0].image;
+            $cardArea.append(
+                $('<img>', {
+                    src: cardSrc,
+                })
+            );
+            if (cardData.remaining === 0) $button.remove();
+        });
+    }
+    part3();
+});
